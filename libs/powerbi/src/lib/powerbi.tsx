039a438b2627e7057588b8c4da5901959ae6d7f4
Alec Goldis <agoldis@trimontrea.com>
2022-09-12 21:25:46 -0400

    initial

---
+++ libs/powerbi/src/lib/powerbi.tsx
@@ -0,0 +1,23 @@
+import { Route, Link } from 'react-router-dom';
+
+import styles from './powerbi.module.css';
+
+/* eslint-disable-next-line */
+export interface PowerbiProps {}
+
+export function Powerbi(props: PowerbiProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Powerbi!</h1>
+
+      <ul>
+        <li>
+          <Link to="/">powerbi root</Link>
+        </li>
+      </ul>
+      <Route path="/" element={<div>This is the powerbi root route.</div>} />
+    </div>
+  );
+}
+
+export default Powerbi;
