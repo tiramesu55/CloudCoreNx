0879df195f32c420898f7faf4773e246dd9abf84
Alec Goldis <agoldis@trimontrea.com>
2022-09-17 18:53:01 -0400

    cleanup

--- apps/analytics/src/app/app.tsx
+++ apps/analytics/src/app/app.tsx
@@ -8,36 +8,13 @@ import { OktaCode, useClaimsAndSignout } from '@cloudcore/okta-and-config';
 import { AnalyticsPowerbi } from '@cloudcore/analytics/powerbi';
 
 export function App() {
-  //const {signOut, getClaims } = useClaimsAndSignout( "xxxs","yyy");
   return (
-    <>
+
     <Routes>
      <OktaCode oidc={{    "issuer": "https://iarx-services.oktapreview.com/oauth2/default/",
     "clientId": "0oa2e7f4dvYLDDdmw1d7", "redirectUri": "http://localhost:3000/login/callback"}} router={AnalyticsPowerbi} />
     </Routes>
-      {/* <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route path="/powerbi" element={<AnalyticsPowerbi />} />
-        <Route path="/powerbi" element={<Powerbi />} />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes> */}
-      {/* END: routes */}
-    </>
+
   );
 }
 
