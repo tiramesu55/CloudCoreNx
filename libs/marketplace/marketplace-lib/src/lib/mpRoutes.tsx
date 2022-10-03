dbcf795fff4d74e2652e134a43faa8004b3365a7
Alec Goldis <agoldis@trimontrea.com>
2022-10-02 22:56:44 -0400

    handle anauth in Marketplace

--- libs/marketplace/marketplace-lib/src/lib/mpRoutes.tsx
+++ libs/marketplace/marketplace-lib/src/lib/mpRoutes.tsx
@@ -3,7 +3,7 @@ import {  useContext } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
 import { ConfigCtx, IConfig, useClaimsAndSignout} from '@cloudcore/okta-and-config';
-import {Header} from '@cloudcore/ui-shared'
+import {Header, NotAuthorized} from '@cloudcore/ui-shared'
 import { useHistory } from "react-router-dom";
 import Component1 from "../components/component1/component1";
 import Component2 from "../components/component2/component2";
@@ -15,9 +15,14 @@ export const MpRoutes = () => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const config: IConfig  = useContext(ConfigCtx)!;   // at this point config is not null (see app)
   const {signOut, initials, names, permissions } = useClaimsAndSignout( config.logoutSSO,config.postLogoutRedirectUri);
-  console.log( permissions.marketplace ?"allowed to marketplace" :"no marketplace permissions. Redirect to notauthorized")
+
+  const mpPermissions =   permissions.marketplace?.length > 0;
+  console.log(mpPermissions)
   const history = useHistory();
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+    { mpPermissions  ?
     <>
           <Header
             title={'MARKETPLACE'}
@@ -64,8 +69,9 @@ export const MpRoutes = () => {
                     component={Component2}
                   />
           </Switch>
-          </>
-       
+     </>: <NotAuthorized signOut={signOut} />
+    }
+       </>
       ) 
 
 };
